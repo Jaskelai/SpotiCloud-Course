6dc0878c86ee615f91d94922228c143abc7963ac
KornilovMikhail <kornilovmisha1999@gmail.com>
2019-04-02 18:11:44 +0300

    add detekt for circleci

--- app/src/main/java/com/github/kornilovmikhail/spoticloud/ui/MainActivity.kt
+++ app/src/main/java/com/github/kornilovmikhail/spoticloud/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.kornilovmikhail.spoticloud
+package com.github.kornilovmikhail.spoticloud.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kornilovmikhail.spoticloud.R
 
 class MainActivity : AppCompatActivity() {
 
